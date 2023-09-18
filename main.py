c03a4e211f2fed716e25482e3bb5b7991e20465a
Elliott Rahming <145372258+Elliott242@users.noreply.github.com>
2023-09-18 12:28:59 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+string1 = "Python"
+string2 = "programming"
+print(string1 + string2)
+print("I want to learn more about " + string1 + " " + string2)
+print(
+    "C sucks, going from python to C and then back to python multiple times a week is going to make me sick"
+)
